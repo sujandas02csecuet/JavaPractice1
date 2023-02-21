6fc1c66b9bbddb3133db24c305d0cd63ebffce93
sujan das <SUJANDAS02CSECUET@GMAIL.COM>
2023-02-21 19:50:55 +0600

    rr

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Joy Sree Rama");
+        System.out.println("Joy " +
+                "Sree Rama");
     }
 }
